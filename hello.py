f39c8993681e32d6ce48fde3c7ecb98698affaea
Tianying Xia <xiaty1@gmail.com>
2021-09-25 22:29:41 -0600

    Re-structured; major modification of hello.py and login.py

--- hello.py
+++ hello.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
-import os, json
-from templates import login_page, secret_page, after_login_incorrect
-import secret
+import os, json, cgi, cgitb
+from templates import login_page
 
 print('Content-type:text/html\r\n\r\n')
-print
+print()
+print('<html>')
+print('<head>')
 print('<title>Test CGI</title>')
+print('</head>')
+print('<body>')
+print("<H1>This is my first CGI script</H1>")
 print('<p>Hello world.</p>')
+print('</body>')
+print('</html>')
 
 
+'''
 # Q1 
 #print(os.environ)
 json_object = json.dumps(dict(os.environ), indent=4)
 print(json_object)
-
+'''
 
 # Q2 
 for p in os.environ.keys():
@@ -27,4 +34,10 @@ for p in os.environ.keys():
         print( '<b>%20s</b>: %s<br>' % (p, os.environ[p]) )
 
 
-print(login_page())
+# Q4 + 
+for p in os.environ.keys():
+    if (p == 'HTTP_COOKIE'):
+        print( '<b>%20s</b>: %s<br>' % (p, os.environ[p]) )
+
+
+print( login_page() )
--- login.py
+++ login.py
@@ -2,38 +2,55 @@
 import cgi, cgitb, os
 from templates import login_page, secret_page, after_login_incorrect
 import secret
+import http.cookies
 
 
+cookie_string = os.environ.get('HTTP_COOKIE')
+#print(cookie_string)
 
-def main(): 
-    cgitb.enable()
-    
-    # Create instance of FieldStorage 
+if not cookie_string:
     form = cgi.FieldStorage()
-    
-    #Get data from fields 
-    username = form.getvalue('username')
-    password = form.getvalue('password')
-    
-    print('Content-type: text/html\r\n\r\n')
-    print('<html>')
-    print('<head>')
-    print('<title>Login - CGI lab</title>')
-    print('</head>')
-    print('<body>')
-    print('<p><b>Username</b> %s <b>password</b> %s </p>' % (username, password))
-    print('</body>')
-    print('</html>')
-        
-    print('Set-Cookie: ID = %s' % username)
-    print('Set-Cookie: Password = %s' % password)
-    
-    if username and password:
+    if len(form) >= 2:
+        username = form.getvalue('username')
+        password = form.getvalue('password')
+        '''
+        print('<html>')
+        print('<head>')
+        print('<title>Login - CGI lab</title>')
+        print('</head>')
+        print('<body>')
+        print('<p><b>Username</b> %s <b>password</b> %s </p>' % (username, password))
+        print('</body>')
+        print('</html>')
+        '''
         if username == secret.username and password == secret.password:
+            print('Set-Cookie: Username=%s' % username)
+            print('Set-Cookie: Password=%s' % password)
+            print('Content-type:text/html\r\n\r\n')
             page_next = secret_page(username, password)
             print(page_next)
         else:
+            print('Content-type:text/html\r\n\r\n')
             page_next = after_login_incorrect()
             print(page_next)
+    
+    else:
+        print('Content-type:text/html\r\n\r\n')
+        print(login_page())
 
-main()
+else:
+    cookie = http.cookies.SimpleCookie()
+    cookie.load(cookie_string)
+    username = cookie['Username'].value
+    password = cookie['Password'].value
+    if username == secret.username and password == secret.password:
+        print('Set-Cookie: Username=%s' % username)
+        print('Set-Cookie: Password=%s' % password)
+        print('Content-type:text/html\r\n\r\n')
+        page_next = secret_page(username, password)
+        print(page_next)
+    else:
+        print('Content-type:text/html\r\n\r\n')
+        page_next = after_login_incorrect()
+        print(page_next)
+        print('<p>Wrong information stored in the cookie; delete the cookie, then try again.</p>')
